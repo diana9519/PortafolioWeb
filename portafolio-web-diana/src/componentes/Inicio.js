cd7cdc7ca141e4bae8aeb45f6a6703ac8578deee
diana9519 <94394943+diana9519@users.noreply.github.com>
2023-12-29 11:36:05 -0500

    Culminación del diseño del Inicio

--- portafolio-web-diana/src/componentes/Inicio.js
+++ portafolio-web-diana/src/componentes/Inicio.js
@@ -1,10 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '../styles/Inicio.module.css'; // Importa los estilos modulares específicos para Inicio
-import pcImage from './../assets/pc.gif';
+import pcImage from './../assets/prog.gif';
 import Menu from './Menu';
 
 const Inicio = () => {
   const [mostrarMenu, setMostrarMenu] = useState(false);
+  const [textToShow, setTextToShow] = useState('');
+  const text = 'Soy Diana Pinchao ...';
+  const delay = 100; // Puedes ajustar la velocidad de la animación
+  const pauseTime = 2000; // Tiempo de pausa entre animaciones (en milisegundos)
+
+  useEffect(() => {
+    let i = 0;
+    let isDeleting = false;
+
+    const typeEffect = () => {
+      const currentText = text.substring(0, i);
+      setTextToShow(currentText);
+
+      if (!isDeleting && i === text.length) {
+        isDeleting = true;
+        setTimeout(typeEffect, pauseTime);
+      } else if (isDeleting && i === 0) {
+        isDeleting = false;
+        setTimeout(typeEffect, delay);
+      } else if (isDeleting) {
+        i--;
+        setTimeout(typeEffect, delay);
+      } else {
+        i++;
+        setTimeout(typeEffect, delay);
+      }
+    };
+
+    typeEffect();
+
+    return () => clearTimeout();
+  }, [text, delay, pauseTime]);
+
   const handleRedirect = () => {
     setMostrarMenu(true);
   };
@@ -13,9 +46,11 @@ const Inicio = () => {
     <div className={styles.InicioBackground}>
       {!mostrarMenu ? (
         <div className={styles.InicioContent}>
-          <img id="pc" src={pcImage} alt="Culebrita" />
-          <div className={styles.InicioTitleContainer}>
-            <h1>Portafolio Diana Pinchao</h1>
+          <h1>{textToShow}</h1>
+          <div className={styles.InicioImageContainer}>
+            <img id="pc" src={pcImage} alt="Pc" className={styles.InicioImage} />
+          </div>
+          <div className={styles.InicioTextContainer}>
             <button className={styles.InicioStartButton} onClick={handleRedirect}>
               Empezar
             </button>
